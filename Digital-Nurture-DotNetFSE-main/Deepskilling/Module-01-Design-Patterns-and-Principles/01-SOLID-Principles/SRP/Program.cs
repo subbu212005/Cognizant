99e22f67f9567e738df20f53dfd4bc4cad2b9a86
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-17 11:15:59 +0530

    Update Program.cs

--- Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/SRP/Program.cs
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/SRP/Program.cs
@@ -1,1 +1,26 @@
+using System;
 
+class Book
+{
+    public string Title { get; set; } = "";
+}
+
+class BookPrinter
+{
+    public void Print(Book book)
+    {
+        Console.WriteLine(book.Title);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Book b = new Book();
+        b.Title = "C# Programming is very helpful for learning design principles.";
+
+        BookPrinter printer = new BookPrinter();
+        printer.Print(b);
+    }
+}
