5089009b70a07e39045bc8314293d75abaa643b6
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-18 11:10:47 +0530

    Update program.cs

--- Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/ISP/program.cs
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/ISP/program.cs
@@ -1,1 +1,23 @@
+using System;
 
+interface IPrint
+{
+    void Print();
+}
+
+class Printer : IPrint
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing Document...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Printer printer = new Printer();
+        printer.Print();
+    }
+}
