edde025b89613649d5e255da1f40efd4b4d3048f
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-18 09:30:52 +0530

    Update program.cs

--- Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/LSP/program.cs
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/LSP/program.cs
@@ -1,1 +1,26 @@
+using System;
 
+class Bird
+{
+    public virtual void Move()
+    {
+        Console.WriteLine("Bird moves");
+    }
+}
+
+class Sparrow : Bird
+{
+    public override void Move()
+    {
+        Console.WriteLine("Sparrow flies");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Bird bird = new Sparrow();
+        bird.Move();
+    }
+}
