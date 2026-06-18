f1952f843a328c97c675cd959f0f882dbc9e0383
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-18 09:14:02 +0530

    Update progam.cs

--- Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/OCP/progam.cs
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-01-Design-Patterns-and-Principles/01-SOLID-Principles/OCP/progam.cs
@@ -1,1 +1,30 @@
+using System;
 
+class Shape
+{
+    public virtual double Area()
+    {
+        return 0;
+    }
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public override double Area()
+    {
+        return 3.14 * Radius * Radius;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle();
+        circle.Radius = 5;
+
+        Console.WriteLine("Area of Circle: " + circle.Area());
+    }
+}
