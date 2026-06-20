2f0e0930a677fd3cc7c2a43d9e580a76f95fcdb8
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-20 09:54:45 +0530

    Create program.cs

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-02-Data-Structures-and-Algorithms/Arrays/program.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr={10,20,30,40,50};
+
+        Console.WriteLine("Array Elements:");
+
+        foreach (int item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
