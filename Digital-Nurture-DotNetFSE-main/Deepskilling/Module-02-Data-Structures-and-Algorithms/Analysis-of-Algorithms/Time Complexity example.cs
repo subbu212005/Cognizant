afb98558f6e359a80e774e976cdcf3d395c9b3fe
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-20 09:20:20 +0530

    Create Time Complexity example.cs

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-02-Data-Structures-and-Algorithms/Analysis-of-Algorithms/Time Complexity example.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 5;
+
+        Console.WriteLine("Numbers from 1 to " + n);
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Time Complexity: O(n)");
+    }
+}
