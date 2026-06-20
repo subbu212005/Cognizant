24c290d7f3316c98c0889a5e485868a4ac9435de
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-20 09:59:53 +0530

    Create program.cs

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-02-Data-Structures-and-Algorithms/Arrays/Array Searching (Linear Search)/program.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr={10,20,30,40,50 };
+        int key=30;
+
+        bool found=false;
+
+        for (int i=0;i<arr.Length;i++)
+        {
+            if (arr[i]==key)
+            {
+                Console.WriteLine("Element Found at Index: " + i);
+                found=true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Element Not Found");
+        }
+    }
+}
