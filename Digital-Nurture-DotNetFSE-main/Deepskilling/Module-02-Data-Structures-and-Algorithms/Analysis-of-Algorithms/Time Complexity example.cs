9f91f6a46e758e17e6d459f9d3df807744a1466c
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-20 09:22:45 +0530

    Update Time Complexity example.cs

--- Digital-Nurture-DotNetFSE-main/Deepskilling/Module-02-Data-Structures-and-Algorithms/Analysis-of-Algorithms/Time Complexity example.cs
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-02-Data-Structures-and-Algorithms/Analysis-of-Algorithms/Time Complexity example.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main()
     {
-        int n = 5;
+        int n=5;
 
         Console.WriteLine("Numbers from 1 to " + n);
 
-        for (int i = 1; i <= n; i++)
+        for (int i=1;i<=n;i++)
         {
             Console.WriteLine(i);
         }
