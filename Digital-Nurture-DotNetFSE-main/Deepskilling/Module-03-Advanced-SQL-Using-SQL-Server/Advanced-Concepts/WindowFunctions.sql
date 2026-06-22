539a3e7855929b82281eb97b0fae2bdce94a3960
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-06-22 23:01:31 +0530

    Create WindowFunctions.sql

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-03-Advanced-SQL-Using-SQL-Server/Advanced-Concepts/WindowFunctions.sql
@@ -0,0 +1,12 @@
+SELECT
+    StudentID,
+    StudentName,
+    Marks,
+
+    ROW_NUMBER() OVER(ORDER BY Marks DESC) AS RowNumber,
+
+    RANK() OVER(ORDER BY Marks DESC) AS RankValue,
+
+    DENSE_RANK() OVER(ORDER BY Marks DESC) AS DenseRankValue
+
+FROM Students;
