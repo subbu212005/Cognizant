97f231aa7f4a3fc6d65b337b11dfc23ac81dbcc6
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-10 15:06:34 +0530

    Create program.cs

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-05-Entity-Framework-Core-8.0/Hands-On-Exercises/EF-Core-8.0/Lab-01-Understanding-ORM/RetailInventory/program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retail Inventory System");
+            Console.WriteLine();
+
+            Console.WriteLine("Project Created Successfully.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Entity Framework Core Packages Installed.");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ready to connect SQL Server.");
+        }
+    }
+}
