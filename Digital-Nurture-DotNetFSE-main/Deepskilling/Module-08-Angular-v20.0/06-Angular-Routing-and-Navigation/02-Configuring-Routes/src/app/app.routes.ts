229a68d8f380d879990be35f5d447ba5f458df4d
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-20 23:18:34 +0530

    Create app.routes.ts

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/06-Angular-Routing-and-Navigation/02-Configuring-Routes/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { AboutComponent } from './about/about';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: 'home' }
+];
