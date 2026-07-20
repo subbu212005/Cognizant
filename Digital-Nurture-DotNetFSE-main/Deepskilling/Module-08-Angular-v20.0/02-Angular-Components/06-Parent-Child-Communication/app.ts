720876149dc5cc1e35d2c34b7f35492c5745c526
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-20 22:43:13 +0530

    Create app component with student component import

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/02-Angular-Components/06-Parent-Child-Communication/app.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { StudentComponent } from './student/student';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [StudentComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+  name = "John Doe";
+
+}
