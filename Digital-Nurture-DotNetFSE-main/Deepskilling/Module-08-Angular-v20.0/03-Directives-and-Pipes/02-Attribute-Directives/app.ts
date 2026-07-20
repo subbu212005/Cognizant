9147b87035a2b0619563adb13d29c6363f778347
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-20 22:51:56 +0530

    Create app.ts

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/03-Directives-and-Pipes/02-Attribute-Directives/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { StudentComponent } from './student/student';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [StudentComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+}
