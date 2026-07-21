e614a88e5ab2fa597e91d18f4c45f6bb032b8729
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-21 19:06:11 +0530

    Add files via upload

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/07-HTTP-Client-and-APIs/01-Introduction-to-HTTPClient/src/app/student/student.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  templateUrl: './student.html',
+  styleUrl: './student.css'
+})
+export class StudentComponent { }
