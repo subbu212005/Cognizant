22d98a384930696917f234ae691a8836e5e8ddc0
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-21 14:11:12 +0530

    Create home.ts

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/06-Angular-Routing-and-Navigation/05-Lazy-Loading/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.html',
+  styleUrl: './student.css'
+})
+export class HomeComponent {
+
+}
