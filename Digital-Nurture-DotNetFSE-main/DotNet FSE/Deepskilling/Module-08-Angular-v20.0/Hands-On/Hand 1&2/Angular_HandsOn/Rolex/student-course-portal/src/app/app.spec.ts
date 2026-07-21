a85ec3213cadf15b978642e4e04499462031d78c
Subbu <subbu@example.com>
2026-07-21 23:43:35 +0530

    feat: add Angular Hands-On 1 & 2 under Rolex

---
+++ Digital-Nurture-DotNetFSE-main/DotNet FSE/Deepskilling/Module-08-Angular-v20.0/Hands-On/Hand 1&2/Angular_HandsOn/Rolex/student-course-portal/src/app/app.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideRouter([])]
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have the title signal set correctly', () => {
+    const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    expect(app.title()).toEqual('student-course-portal');
+  });
+});
+
