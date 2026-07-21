f7b6329314c4a88666b82f3ee93e327b05068cf0
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-21 14:12:38 +0530

    Create contact.ts

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/06-Angular-Routing-and-Navigation/05-Lazy-Loading/contact.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  templateUrl: './contact.html',
+  styleUrl: './student.css'
+})
+export class ContactComponent {
+
+}
