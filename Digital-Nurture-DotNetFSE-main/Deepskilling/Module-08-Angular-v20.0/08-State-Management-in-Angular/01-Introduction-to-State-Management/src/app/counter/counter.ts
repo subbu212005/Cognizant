5b0f1f2bffb224e3fc65d5dc1db9a98350239aeb
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-21 19:20:22 +0530

    Add files via upload

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-08-Angular-v20.0/08-State-Management-in-Angular/01-Introduction-to-State-Management/src/app/counter/counter.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-counter',
+  standalone:true,
+  templateUrl:'./counter.html',
+  styleUrl:'./counter.css'
+})
+export class CounterComponent{
+  count=0;
+  increment(){
+    this.count++;
+  }
+}
