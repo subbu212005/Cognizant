48c5ed4178938a4fb41e88e8421989f6a2691673
Yedula Subrahmanyeswara <yedula2005@gmail.com>
2026-07-24 22:19:28 +0530

    Add files via upload

---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-05-Entity-Framework-Core-8.0/02-Setting-up-EF-Core-in-a-.NET-8-Project/code/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using EFCoreSetupDemo.Models;
+
+namespace EFCoreSetupDemo.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Student> Students => Set<Student>();
+}
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
---
+++ Digital-Nurture-DotNetFSE-main/Deepskilling/Module-05-Entity-Framework-Core-8.0/02-Setting-up-EF-Core-in-a-.NET-8-Project/code/Models/Student.cs
@@ -0,0 +1,8 @@
+namespace EFCoreSetupDemo.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
